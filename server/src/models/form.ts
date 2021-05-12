ef7a5759b56212d6589e4b594fe2fa779c7df24d
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-05-12 16:27:13 +0200

    create new files

---
+++ server/src/models/form.ts
@@ -0,0 +1,10 @@
+class NewForm {
+    id:string;
+    action:string;
+    timestamp:string;
+    constructor(id:string, action:string, timestamp:number, ) {
+        this.id = id
+        this.action = id
+        this.timestamp = id
+    }
+}
---
+++ server/src/serverConfig.ts
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+    PORT,
+    HOST,
+    HOST_URL,
+    API_KEY,
+    AUTH_DOMAIN,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID,
+    MEASUREMENT_ID
+} = process.env;
+
+console.assert(PORT, 'PORT is require');
+console.assert(HOST, 'HOST is require');
+
+export default {
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
+    firebaseConfig: {
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID,
+        measurementId: MEASUREMENT_ID
+    }
+}
