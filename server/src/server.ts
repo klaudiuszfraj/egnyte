56be3325196d8cc6db6bf9e462824e00e2282198
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-05-13 17:14:03 +0200

    add cors and socket.io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,34 @@
 import express from 'express';
-import io from 'socket.io';
+import {createServer} from "http";
+import { Server } from 'socket.io';
+import cors from 'cors';
 import serverConfig from './serverConfig';
 import api from "./routes/fromRoute";
 
 const PORT = 8080 || serverConfig.port
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: ['http://localhost:3000', 'https://localhost:3000']
+    }
+});
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/form', api);
 
 app.get('/', ((req, res) => {
     res.send('hello')
 }));
+io.on('connection', () => {
+    console.log('connected')
+})
 
-app.listen(PORT, ()=>console.log(`server is running on ${PORT}`));
+httpServer.listen(PORT, ()=>console.log(`server is running on ${PORT}`));
+
