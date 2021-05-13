125344383f60e1bcae81b19d0857cbf4b9889788
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-05-13 20:06:18 +0200

    add SocketProvider and FormsWrapper

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/Header/Header";
+import SocketProvider from "./context/SocketProvider";
+import FormsWrapper from "./components/Froms/FormsWrapper";
 
 function App() {
   return (
     <>
         <Header/>
+        <SocketProvider>
+            <FormsWrapper/>
+        </SocketProvider>
     </>
   );
 }
