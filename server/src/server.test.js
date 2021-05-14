ac29b6dfb9797ff6212343ee56d5ca25759b3cd7
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-05-14 16:20:17 +0200

    create test

---
+++ server/src/server.test.js
@@ -0,0 +1,44 @@
+import {createServer} from "http";
+import { Server } from 'socket.io';
+import Client from "socket.io-client";
+
+
+describe("egnyte server", () => {
+    let io, serverSocket, clientSocket;
+    const port = 8080
+    beforeAll((done) => {
+        const httpServer = createServer();
+        io = new Server(httpServer);
+        httpServer.listen(() => {
+            const port = httpServer.address().port;
+            clientSocket = new Client(`http://localhost:${port}`);
+            io.on("connection", (socket) => {
+                serverSocket = socket;
+            });
+            clientSocket.on("connect", done);
+        });
+    });
+
+    // afterAll(() => {
+    //     io.close();
+    //     clientSocket.close();
+    // });
+
+    // test("should work", (done) => {
+    //     clientSocket.on("hello", (arg) => {
+    //         expect(arg).toBe("world");
+    //         done();
+    //     });
+    //     serverSocket.emit("hello", "world");
+    // });
+
+    // test("should work (with ack)", (done) => {
+    //     serverSocket.on("hi", (cb) => {
+    //         cb("hola");
+    //     });
+    //     clientSocket.emit("hi", (arg) => {
+    //         expect(arg).toBe("hola");
+    //         done();
+    //     });
+    // });
+});
