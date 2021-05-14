98ea698b8752f2e1d50e6cde5b1850ac7e177a14
Klaudiusz Frajkowski <klaudiusz.fraj@gmail.com>
2021-05-14 13:24:31 +0200

    dispraly radio and change functionality

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -3,22 +3,34 @@ import Input from "../Input/Input";
 import {InterfaceForm} from "../../interfaces";
 import {useSocket} from "../../context/SocketProvider";
 
-const Form:React.FC<InterfaceForm> = ({ timestamp, fields, id  }) => {
+const Form:React.FC<InterfaceForm> = ({ timestamp, fields, radio, id  }) => {
     const socket = useSocket()
     const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
-
         const fieldsCopy = [...fields];
-        fieldsCopy.map(field => {
-            if (field.name === e.target.name) {
-                field.checked = e.target.checked
-            }
-        })
+        const radioCopy = [...radio];
+        if (e.target.type === 'checkbox') {
+            fieldsCopy.map(field => {
+                if (field.name === e.target.name) {
+                    field.checked = e.target.checked
+                }
+            })
+        } else {
+            radioCopy.map(radio => {
+                if (radio.id === e.target.id) {
+                    console.log('fff')
+                    radio.checked = true
+                } else {
+                    radio.checked = false;
+                }
+            })
+        }
         if (socket) {
             socket.emit('edit', {
                 formId: id,
                 update: {
-                    timestamp: 545,
-                    fields: fieldsCopy
+                    timestamp: new Date().getTime(),
+                    fields: fieldsCopy,
+                    radio: radioCopy,
                 }
             })
         }
@@ -27,13 +39,28 @@ const Form:React.FC<InterfaceForm> = ({ timestamp, fields, id  }) => {
         <form action="" id={id} data-timestamp={timestamp}>
             {fields.map(field => (
                 <Input
-                    key={field.name}
+                    id={'d'}
+                    key={field.id}
                     type={field.type}
                     name={field.name}
                     value={field.value}
                     checked={field.checked}
                     handleChange={handleClick}
                 />
+            ))}
+            <br/>
+            {radio.map(radio => (
+                <Input
+                    id={radio.id}
+                    key={radio.id}
+                    type={radio.type}
+                    name={radio.name}
+                    value={radio.value}
+                    checked={radio.checked}
+                    handleChange={handleClick}
+
+                />
+
             ))}
         </form>
     );
